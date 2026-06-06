dd36e49328d02c5e5f80480cfd39f3e7130227e4
francoyimer2-collab <francoyimer2@gmail.com>
2026-06-06 03:09:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
+
 
 const vehiculosRoutes = require("./routes/vehiculos");
 const clientesRoutes = require("./routes/clientes");
