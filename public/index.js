bcf61e902c4f24567e0546008b297924c7630a18
francoyimer2-collab <francoyimer2@gmail.com>
2026-06-06 03:30:02 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 // ─── CONFIGURACIÓN ────────────────────────────────────────
-const API_URL = "http://localhost:3000";
+const API_URL = "";
 
 // ─── NAVEGACIÓN ───────────────────────────────────────────
 
